39763ccb57915713d137118da185fa857d67dc5c
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2021-10-20 10:35:31 +0500

    Update index.js

--- Unpaid-Task/index.js
+++ Unpaid-Task/index.js
@@ -1,30 +1,45 @@
-// side nav tab
-function openTab(evt, tabCol) {
-  // Declare all variables
-  var i, tabCol, tablinks;
+// // side nav tab
+// function openTab(evt, tabCol) {
+//   // Declare all variables
+//   var i, tabCol, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tab-col");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+//   // Get all elements with class="tabcontent" and hide them
+//   tabcontent = document.getElementsByClassName("tab-col");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+//   // Get all elements with class="tablinks" and remove the class "active"
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabCol).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+//   // Show the current tab, and add an "active" class to the button that opened the tab
+//   document.getElementById(tabCol).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
 
-// side-nav toggle
-function openNav() {
-  document.getElementById("side-nav").style.width = "100%";
-}
+// // side-nav toggle
+// function openNav() {
+//   document.getElementById("side-nav").style.width = "100%";
+// }
 
-function closeNav() {
-  document.getElementById("side-nav").style.width = "0";
+// function closeNav() {
+//   document.getElementById("side-nav").style.width = "0";
+// }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
